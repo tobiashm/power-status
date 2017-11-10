c8f20c6df01a08248f7d5d69a7af848b7f1a5c31
Tobias Haagen Michaelsen <tobias.michaelsen@gmail.com>
2017-11-10 11:12:55 +0100

    Create new Luxafor connection for every change
    Keep state so we don't need to set color every time.

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const osxBattery = require('osx-battery');
 const Luxafor = require('luxafor-api');
 
-const device = new Luxafor();
+let currentState = null;
 
-const checkIsCharging = () => osxBattery().then(res => {
-  const color = res.externalConnected ? (res.isCharging ? '#00f' : '#fff') : '#f00';
-  device.setColor(color);
+const resolveState = res => res.externalConnected ? (res.isCharging ? 'charging' : 'full') : 'discharging';
+
+const colors = {
+  charging: '#00f',
+  discharging: '#f00',
+  full: '#fff'
+};
+
+const checkIsCharging = () => osxBattery().then(resolveState).then(nextState => {
+  if (nextState === currentState) return;
+  currentState = nextState;
+  const device = new Luxafor();
+  device.setColor(colors[currentState] || '#000');
 });
 
 checkIsCharging();
