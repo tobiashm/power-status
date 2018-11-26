68a0c0a789ad8733530a0136786f0d737e28fa13
Tobias Haagen Michaelsen <tobias.michaelsen@gmail.com>
2018-11-26 09:31:35 +0100

    Simplify; don't try to be clever
    Instead of trying to delay creation of Luxafor instances etc., we now just create a new device and set the current colors on each update.
    
    Some of the reason for the early returns etc. before was because we'd also blink the lights when the state changed. Since we no longer do that, the code can be simplified a lot.

--- index.js
+++ index.js
@@ -1,47 +1,26 @@
 const osxBattery = require('osx-battery');
 const Luxafor = require('luxafor-api');
 
-const device = new Luxafor();
-
-let currentState = null;
-let connected = null;
-
 const resolveState = res => res.fullyCharged ? 'full' : (
   res.isCharging ? 'charging' : 'discharging'
 );
 
-const colors = {
+const stateColors = {
   charging: '#00f',
   discharging: '#f00',
   full: '#fff'
 };
 
-const targets = {
-  all: 0xff,
-  top: 0x41,
-  bottom: 0x42,
-};
+const stateColor = res => stateColors[resolveState(res)] || '#000';
 
-const checkConnection = ({ externalConnected }) => new Promise(resolve => {
-  if (connected === externalConnected) return resolve();
-  connected = externalConnected;
-  const color = connected ? '#0f0' : '#f00';
-  device.setColor(color, targets.bottom);
-  resolve();
-});
-
-const checkState = (res) => new Promise(resolve => {
-  const nextState = resolveState(res);
-  if (nextState === currentState) return resolve();
-  currentState = nextState;
-  device.setColor(colors[currentState] || '#000', targets.top);
-  resolve();
-});
+const connectedColor = res => res.externalConnected ? '#0f0' : '#f00';
 
 const checkIsCharging = () => osxBattery().then(res => {
-  checkConnection(res);
-  checkState(res);
+  const device = new Luxafor();
+  const targets = device.getTargets();
+  device.setColor(connectedColor(res), targets.bottom);
+  device.setColor(stateColor(res), targets.top);
 });
 
-device.off();
-setInterval(checkIsCharging, 1000);
+new Luxafor().off();
+setInterval(checkIsCharging, 3000);
